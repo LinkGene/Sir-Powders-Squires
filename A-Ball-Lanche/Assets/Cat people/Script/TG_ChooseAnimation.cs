3df4ddcc480eff3eb16c29eb4281799df451b8c8
ZephyrVat <jojohn210@hotmail.com>
2023-02-13 22:35:05 -0800

    Cat peoples
    added cat peoples

---
+++ A-Ball-Lanche/Assets/Cat people/Script/TG_ChooseAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TG_ChooseAnimation : MonoBehaviour {
+
+    public string playAnimationName;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Animator>().Play(playAnimationName, 0);
+	}
+
+}
